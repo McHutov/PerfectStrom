dcadfd4b7a516cb87f633796cc207461ea3dbb08
Dastan Makhutov <dos.breakdancer@mail.ru>
2020-07-29 22:34:06 +0600

    HomeWork lesson7

---
+++ src/lesson7/ProgramTask75.java
@@ -0,0 +1,19 @@
+package lesson7;
+
+import java.util.Scanner;
+
+public class ProgramTask75
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        int a;
+        int sum = 0;
+        do
+        {
+            a = s.nextInt();
+            sum+=a;
+        }while(a != 0);
+        System.out.println(sum);
+    }
+}
