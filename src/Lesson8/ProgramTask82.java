9a32f314edaf766f683e73e76e850a32fad7046f
Dastan Makhutov <dos.breakdancer@mail.ru>
2020-08-02 23:49:55 +0600

    Lesson8 HomeWork

---
+++ src/Lesson8/ProgramTask82.java
@@ -0,0 +1,45 @@
+package Lesson8;
+
+import java.util.Scanner;
+
+public class ProgramTask82
+{
+    public static void main(String args[])
+    {
+        System.out.println(method1(5));
+        System.out.println(method1(0));
+        System.out.println(method2(5));
+        System.out.println(method2(0));
+        method3(5);
+        method3(0);
+    }
+
+    static boolean method1(int a)
+    {
+        if( a > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    static String method2(int a)
+    {
+        if(method1(a))
+        {
+            return "Numer is greater then zero";
+        }
+        else
+        {
+            return "number is zero or less";
+        }
+    }
+
+    static void method3(int x)
+    {
+        System.out.println(method1(x)+ " and equal to " + x);
+    }
+}
